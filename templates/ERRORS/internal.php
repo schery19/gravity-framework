2f58ee968616a5f44caf374e8c180302a81e8209
schery19 <44877783+schery19@users.noreply.github.com>
2023-05-30 10:40:51 -0400

    Add files via upload

---
+++ templates/ERRORS/internal.php
@@ -0,0 +1,9 @@
+<?php $title = "Erreur !" ?>
+
+<center><h1 style="color: red; margin-bottom: 50px; margin-top: 20px;">Erreur interne !</h1></center>
+
+<div class"container-fluid">
+	<?php echo($error->getMessage()) ?>
+</div>
+
+<center><a role="button" class="btn btn-success btn-lg" style="margin-bottom: 60px;" href="/">Continuer à naviguer</a></center>
