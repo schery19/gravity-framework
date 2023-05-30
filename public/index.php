8814d8aff1459272b315c306b36674d1d44e98c2
schery19 <44877783+schery19@users.noreply.github.com>
2023-05-30 10:03:24 -0400

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ *Le Controlleur frontal du site, c'est à travers ce
+ *fichier que tout passe, on enregistre les différentes
+ *routes supportées par le site afin d'afficher les
+ *différentes vues qui leur sont associées
+ */
+
+require '../vendor/autoload.php';
+require '../config/configs.php';
+
+use Gravity\Core\App\Controllers\Controller;
+use Gravity\Core\Routing\Exceptions\NoRouteException;
+use Gravity\Core\Routing\Exceptions\ControllerException;
+use Gravity\Core\Routing\Router;
+
+$router = new Router($_REQUEST['route']);
+
+//Page d'accueil
+$router->get('/', 'Gravity\Core\App\Controllers\HomeController@index');
+
+/** 
+ *  Ou bien
+ * $router->get('/', function () {
+ *	return (new Controller())->renderView('Home.index','layout');
+ *});
+ *
+ */
+
+
+try {
+	$router->run();
+} catch(NoRouteException $e) {
+	(new Controller())->renderView('Errors.404', 'layout', ['error'=>$e]);
+} catch(ControllerException $e) {
+	(new Controller())->renderView('Errors.internal', 'layout', ['error'=>$e]);
+} 
+
+
+?>
