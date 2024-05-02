3f19688ad5b20eb81e2cac835a8542667ace9aac
schery19 <44877783+schery19@users.noreply.github.com>
2024-05-02 05:21:56 -0400

    Add files via upload

---
+++ configs/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "host" => "localhost",
+    "port" => "3307",
+    "user" => "root",
+    "pass" => "",
+    "dbname" => "gestionblog"
+]
+
+?>
---
+++ templates/Home/home.php
@@ -0,0 +1,5 @@
+<?php $title = "Accueil"; ?>
+
+<h1>First page</h1>
+
+<p>Hello Dear <?= $name ?> !</p>
