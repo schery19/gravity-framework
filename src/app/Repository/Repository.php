8e2880eb378c46e80bd677e970fe4a0eb9cd0b73
schery19 <44877783+schery19@users.noreply.github.com>
2024-10-23 10:45:11 -0400

    Create Repository.php

---
+++ src/app/Repository/Repository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+
+use Gravity\Core\App\Repository\Repository as BaseRepository;
+
+
+class Repository extends BaseRepository {
+
+    public static function update(Entity $entity) {
+        
+        if(in_array('updated_at', static::getColumns())) {
+            $entity->updated_at = static::currentDateTime();
+        }
+
+        return parent::update($entity);
+    }
+    
+}
+
+
+?>
