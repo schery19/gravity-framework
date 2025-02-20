50731e559d3dc38f871e82a3a957ba222a75c778
schery19 <schneiderchery7@gmail.com>
2025-02-20 12:51:22 -0500

    Rendre compatible avec la branche 0.12

--- src/app/Repository/Repository.php
+++ src/app/Repository/Repository.php
@@ -4,19 +4,9 @@ namespace App\Repository;
 
 
 use Gravity\Core\App\Repository\Repository as BaseRepository;
-use Gravity\Core\App\Entity\Entity;
 
 
 abstract class Repository extends BaseRepository {
-
-    public static function update(Entity $entity) {
-        
-        if(in_array('updated_at', static::getColumns())) {
-            $entity->updated_at = static::currentDateTime();
-        }
-
-        return parent::update($entity);
-    }
     
 }
 
