621032e8430a37f2112f4af8cae13d88e122e096
Schery19 <schneiderchery7@gmail.com>
2025-08-08 18:59:58 -0400

    Mettre à jour composer.json pour nécessiter la version 0.15 de gravity-core et modifier la gestion des erreurs dans les templates d'erreur 404 et d'erreur interne.

--- templates/GRAVITY/ERRORS/internal.php
+++ templates/GRAVITY/ERRORS/internal.php
@@ -3,7 +3,7 @@
 <center><h1 style="color: red; margin-bottom: 50px; margin-top: 20px;">Erreur interne !</h1></center>
 
 <div class"container-fluid">
-	<?= isset($error)?$error->getMessage():'' ?>
+	<?= isset($error)?$error:'' ?>
 </div>
 
 <center><a role="button" class="btn btn-success btn-lg" style="margin-bottom: 60px; margin-top: 10px;" href="/">Continuer à naviguer</a></center>
